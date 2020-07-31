27416b8842f973054538ef3bc98bed01fb7ae09a
Erick Medina <erfamel@hotmail.com>
2020-07-31 16:42:21 -0500

    fix price_data (again)

--- src/js/routes.js
+++ src/js/routes.js
@@ -881,7 +881,7 @@ var routes = [
             var app = router.app;
 
             console.log("Membership/confirmed/session/ thing");
-            console.log("0.0.2.15");
+            console.log("0.0.2.16");
 
             var stripeUrl = app.data.stripe.stripeUrl;
             var paymentUrl = app.data.stripe.paymentUrl;
