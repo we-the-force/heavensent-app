a9a8bf54c1f8d912049d613dc72d1aab1567629d
Erick Medina <erfamel@hotmail.com>
2020-07-31 18:25:39 -0500

    fix sub route name

--- src/js/app.js
+++ src/js/app.js
@@ -35,7 +35,7 @@ var app = new Framework7({
         domain: 'https://app.heavensentnow.com',
         stripe: {
             stripeUrl: 'https://api.stripe.com/v1/checkout/sessions',
-            paymsubscriptionUrl: 'https://api.stripe.com/v1/subscriptions',
+            subscriptionUrl: 'https://api.stripe.com/v1/subscriptions',
             memberships:{
                 basic: 'prod_HkB1fcYIm145fD',
                 standard: '',
--- src/js/routes.js
+++ src/js/routes.js
@@ -881,7 +881,7 @@ var routes = [
             var app = router.app;
 
             console.log("Membership/confirmed/session/ thing");
-            console.log("0.0.2.25");
+            console.log("0.0.2.26");
 
             var stripeUrl = app.data.stripe.stripeUrl;
             var subscriptionUrl = app.data.stripe.subscriptionUrl;
