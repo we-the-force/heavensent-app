ee4e0be1be1a3ee9ef94759393c13694d732e400
Erick Medina <erfamel@hotmail.com>
2020-07-31 16:37:32 -0500

    Fix price_data object

--- src/js/routes.js
+++ src/js/routes.js
@@ -881,7 +881,7 @@ var routes = [
             var app = router.app;
 
             console.log("Membership/confirmed/session/ thing");
-            console.log("0.0.2.14");
+            console.log("0.0.2.15");
 
             var stripeUrl = app.data.stripe.stripeUrl;
             var paymentUrl = app.data.stripe.paymentUrl;
