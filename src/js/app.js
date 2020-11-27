bac49a75ced6a7c88cd00da3347f010382a93e60
Will Alvarez <will.alvarez@gmail.com>
2020-11-27 16:23:56 -0600

    localhost to api server

--- src/js/app.js
+++ src/js/app.js
@@ -39,10 +39,10 @@ var app = new Framework7({
     data: {
         // keyConfig
         // testKey: keyConfig.SECRET_KEY,
-        //server: 'https://api.heavensentnow.com',
-        //domain: 'https://app.heavensentnow.com',
-        server: 'http://localhost:1337',
-        domain: 'http://localhost:8080',
+        server: 'https://api.heavensentnow.com',
+        domain: 'https://app.heavensentnow.com',
+        // server: 'http://localhost:1337',
+        // domain: 'http://localhost:8080',
         stripe: {
             stripeApiUrl: 'https://api.stripe.com/v1/',
             stripeUrl: 'https://api.stripe.com/v1/checkout/sessions',
